7ee87ff30bb06cdd73a4f79f069074d840aceadb
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-27 16:20:30 +0100

    add initial tags html and js

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('Hello');
+
+/* 
+esercizio di oggi: password generator (farlocco ed insicurissimo)
+nome repo: js-pwdgen-wannabe
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+Ad esempio se scrivessi:
+fabio
+pacifici
+blue
+la password che esce fuori sará:
+fabiopacificiblue23
+confermate lettura come al solito e buon divertimento con
+Javascript, anzi no, con ECMAScript, anzi no, con ES6...  
+*/
+
+/* Tools
+-prompt
+-(variabile) const/let
+-(per salvare quello che scrive lutente)backticks '${}'
+
+*/
+
+//Chiedi all’utente il suo nome e lo salvo in una variabile
+const user_name = prompt ('Come ti chiami?');
+console.log(user_name);
+
+//poi chiedi il suo cognome
+
+//poi chiedi il suo colore preferito
